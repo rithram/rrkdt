e6e7a71bcf4e14e67e63a5118b09db1377681bea
Parikshit Ram <p.ram@gatech.edu>
2018-11-28 00:53:44 -0500

    Simple sanity check for tree construction + search

---
+++ tests.py
@@ -0,0 +1,12 @@
+def test_tree(S, indexer, locater, hparams) :
+    for i in range(10) :
+        missed_idxs = []
+        tree = indexer(S, hparams)
+        print(tree.keys())
+        for idx, q in enumerate(S) :
+            cset = locater(tree, q)
+            if idx not in cset :
+                print('Index %i, C.set:%s' % (idx, str(cset)))
+                missed_idxs.append(idx)
+        print('Missed %i/%i points' % (len(missed_idxs), len(S)))
+        print(missed_idxs)
