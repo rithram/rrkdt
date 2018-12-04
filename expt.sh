665897506329ed0f3318c4c6bb39c14f7fce855f
Parikshit Ram <p.ram@gatech.edu>
2018-12-04 09:03:20 -0500

    Handle repetition option and timestamped dirs for output

--- expt.sh
+++ expt.sh
@@ -3,6 +3,7 @@
 data_file=$1
 data_type=$2
 output_prefix=$3
+nreps=$4
 
 ks=(10 100)
 muls=(1 2)
@@ -10,12 +11,18 @@ ntrees=50
 
 echo "Data set: $data_file (type: $data_type)"
 
+timestamp=$(date +%Y%m%d.%H%M)
+out_dir="$output_prefix.$timestamp"
+echo "Creating output directory '$out_dir'"
+mkdir -p $out_dir
+
 for k in ${ks[@]}; do
     echo "->Investigating $k-nearest-neighbors problem ..."
     for mul in ${muls[@]}; do
         leaf_size=$(echo "$k * $mul" | bc)
         echo "--> Running experiment with { -N : $leaf_size, -T : $ntrees } ..."
-        prefix="$output_prefix.k$k.N$leaf_size.T$ntrees"
+        echo "----> Performing $nreps repetitions of each setting"
+        prefix="$out_dir/k$k.N$leaf_size.T$ntrees"
         rfile="$prefix.results"
         ffile="$prefix.figures.png"
 
@@ -27,9 +34,10 @@ for k in ${ks[@]}; do
                   -N $leaf_size \
                   -T $ntrees \
                   -r $rfile \
-                  -g $ffile
+                  -g $ffile \
+                  -R $nreps
         echo "-->Results in $rfile and $ffile"
     done
 done
 
-echo "Output written in files prefixed with '$output_prefix'"
+echo "Output files saved in '$out_dir'"
