72f0f62476f932433dd3bf83056c596865eb3d35
Parikshit Ram <p.ram@gatech.edu>
2018-12-05 09:16:57 -0500

    Change Sparse RP-tree setting from (1/3,2/3) to (1/10,1/3)

--- expt_methods.py
+++ expt_methods.py
@@ -24,7 +24,7 @@ def get_methods_for_expt(leaf_size, ntrees) :
     methods.append(rpt_method)
 
     # sparse rptree with {Gaussian, Radamacher} distribution and p={1/3, 2/3}
-    probs = [ (1.0/3.0, '1/3') , (2.0/3.0, '2/3') ]
+    probs = [ (0.1, '1/10') , (1.0/3.0, '1/3') ]
     use_signs = [ (False, 'Ga'), (True, 'Ra') ]
 
     for u, s1 in use_signs :
